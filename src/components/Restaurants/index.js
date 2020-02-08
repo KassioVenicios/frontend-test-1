3668786ab663a0ebd17042eb21cfe0ecc6e2cadd
KassioVenicios <kassio_venicios@hotmail.com>
2020-02-08 18:10:43 -0300

    moved restaurants to utils; location name added in the restaurant 3

--- src/components/Restaurants/index.js
+++ src/components/Restaurants/index.js
@@ -1,66 +1,9 @@
 import React from 'react';
 import RestaurantItem from '../RestaurantItem';
+import restaurants from '../../utils/restaurants';
 import './style.css';
 
 function Restaurants() {
-
-  const restaurants = [{
-    name: 'Very Long Name Restaurants Number 1 In List',
-    rating: 4,
-    category: ['Thai'],
-    openNow: true,
-    priceRange: '$$$$',
-    images: [],
-  }, {
-    name: 'Restaurant 2',
-    rating: 3.5,
-    category: ['Seafood'],
-    openNow: false,
-    priceRange: '$',
-    images: [],
-  }, {
-    name: 'Restaurant 3',
-    rating: 3.5,
-    category: ['Japaneese'],
-    openNow: true,
-    priceRange: '$$$',
-    images: [],
-  }, {
-    name: 'Restaurant 4',
-    rating: 3.5,
-    category: ['Italian'],
-    openNow: true,
-    priceRange: '$$',
-    images: [],
-  }, {
-    name: 'Restaurant 5',
-    rating: 3,
-    category: ['American'],
-    openNow: false,
-    priceRange: '$',
-    images: [],
-  }, {
-    name: 'Restaurant 6',
-    rating: 4,
-    category: ['Mexican'],
-    openNow: true,
-    priceRange: '$$$',
-    images: [],
-  }, {
-    name: 'Restaurant 7',
-    rating: 3,
-    category: ['Steakhouses'],
-    openNow: true,
-    priceRange: '$$$',
-    images: [],
-  }, {
-    name: 'Restaurant 8',
-    rating: 3,
-    category: ['Mexican'],
-    openNow: true,
-    priceRange: '$$$$',
-    images: [],
-  }];
 
   return (
     <section className='container'>
