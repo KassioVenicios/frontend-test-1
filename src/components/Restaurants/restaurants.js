3187f81427a2da123acb36bdf44efee72a0e8eed
KassioVenicios <kassio_venicios@hotmail.com>
2020-02-09 21:29:53 -0300

    restaurants component using class

---
+++ src/components/Restaurants/restaurants.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import RestaurantItem from '../RestaurantItem';
+import './restaurants.style.css';
+
+class Restaurants extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { restaurants } = this.props;
+    return (
+      <section className='container'>
+        <section className='restaurants'>
+          <h2>All Restaurants</h2>
+          <div className='restaurants-list'>
+          {
+            restaurants.map(restaurant => (
+              <RestaurantItem key={restaurant.id} restaurant={restaurant} />
+            ))
+          }
+          </div>
+          <div className='load-more'>
+            Load More
+          </div>
+        </section>
+      </section>
+    );
+  }
+}
+
+export default Restaurants;
