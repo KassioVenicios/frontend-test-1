8b3b7fb8b5f7cf5db8cd6c8cf9f71f5d0ca80fc1
KassioVenicios <kassio_venicios@hotmail.com>
2020-02-09 21:12:27 -0300

    some api improvements

--- src/services/api.js
+++ src/services/api.js
@@ -16,35 +16,47 @@ const api = axios.create({
 const priceFilter = [
   { value: [1,2,3,4], text: 'All' },
   { value: 1, text: '$' },
-  { value: [1, 2], text: '$$' },
-  { value: [1, 2, 3], text: '$$$' },
-  { value: [1, 2, 3, 4], text: '$$$$' },
+  { value: 2, text: '$$' },
+  { value: 3, text: '$$$' },
+  { value: 4, text: '$$$$' },
 ];
 
 const categoriesSearch = async (parent_alias='restaurants') => {
   try {
     const response = await api.get('categories');
-    const categories = response.data.categories.filter(category => {
+    let categories = response.data.categories.filter(category => {
       return category.parent_aliases.includes(parent_alias);
     });
-    return categories.map(category => ({
+    categories = categories.map(category => ({
       value: category.alias,
       text: category.title,
     }))
+    categories.unshift({ value: '', text: 'All' });
+    return categories;
   } catch(error) {
     return console.log(error);
   }
 };
 
-const businessesSearch = async (term, location=defaultLocation, open_now=false, price=priceFilter[0].value) => {
+const businessesSearch = async (searchObj) => {
+  searchObj = searchObj || {};
+  const searchParams = {
+    term: searchObj.term || 'restaurants',
+    location: searchObj.location || defaultLocation,
+    open_now: searchObj.open_now || false,
+    limit: searchObj.limit || defaultLimit,
+    price: searchObj.price || priceFilter[0].value,
+    categories: searchObj.categories || undefined,
+  };
   try {
     let url = 'businesses/search';
-    url = url.concat(`?term=${term}`);
-    url = url.concat(`&location=${location}`);
-    url = url.concat(`&limit=${defaultLimit}`);
-    url = url.concat(`&open_now=${open_now}`);
-    url = url.concat(`&price=${price}`);
-    return await api.get(url);
+    url = url.concat(`?term=${searchParams.term}`);
+    url = url.concat(`&location=${searchParams.location}`);
+    url = url.concat(`&limit=${searchParams.limit}`);
+    url = url.concat(`&open_now=${searchParams.open_now}`);
+    url = url.concat(`&price=${searchParams.price}`);
+    url = url.concat(searchParams.categories ? `&categories=${searchParams.categories}`: '');
+    return api.get(url);
   } catch(error) {
     return console.log(error);
   }
