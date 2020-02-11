9ac80a0e64f410be71495f634a9dcb822bfc3fb4
KassioVenicios <kassio_venicios@hotmail.com>
2020-02-10 21:21:47 -0300

    some fixes in the filter-context

--- src/utils/filter-context.js
+++ src/utils/filter-context.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { priceFilter } from '../services/api';
+import deepCopy from './deepCopy';
 
 export const filters = {
   default: {
@@ -7,12 +8,12 @@ export const filters = {
     location: 'Las Vegas',
     open_now: false,
     limit: 8,
-    price: priceFilter[0],
+    price: priceFilter[0].value,
     categories: undefined,
   },
 };
 
 export const FilterContext = React.createContext({
-  filters: filters.default,
-  applyFilters: () => {},
+  filters: deepCopy(filters.default),
+  changeFilters: (searchObj) => {},
 });
