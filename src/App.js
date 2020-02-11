66b75f6c29281231e0f6d8fe9955417c62d73fba
KassioVenicios <kassio_venicios@hotmail.com>
2020-02-11 01:06:02 -0300

    RestaurantsContext removed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Restaurants from './components/Restaurants/restaurants';
 import RestaurantDetail from './components/RestaurantDetail';
 import restaurantsUtils from './utils/restaurants';
 import { businessesSearch } from './services/api';
-import { RestaurantsContext } from './utils/restaurants-context';
 import { FilterContext, filters } from './utils/filter-context';
 import deepCopy from './utils/deepCopy';
 import './fonts/helvetica-neue.ttf';
@@ -47,22 +46,18 @@ class App extends React.Component {
 
     this.state = {
       restaurants: [],
-      getRestaurants: this.getRestaurants,
       filters: deepCopy(filters.default),
       changeFilters: this.changeFilters,
     };
   }
 
   componentDidMount() {
-    this.state.getRestaurants();
+    this.getRestaurants();
   }
 
   render() {
     return (
-      <RestaurantsContext.Provider value={{
-        restaurants: this.state.restaurants,
-        getRestaurants: this.state.getRestaurants,
-      }}>
+      <>
         <FilterContext.Provider value={{
           filters: this.state.filters,
           changeFilters: this.state.changeFilters,
@@ -74,7 +69,7 @@ class App extends React.Component {
           </main>
           </FilterContext.Provider>
         <RestaurantDetail restaurant={restaurantsUtils[2]} />
-      </RestaurantsContext.Provider>
+      </>
     );
   }
 }
--- src/utils/restaurants-context.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export const RestaurantsContext = React.createContext({
-  restaurants: [],
-  getRestaurants: () => {},
-});
