55ffac6d4f7fa5c395de927b639949280fc043b9
KassioVenicios <kassio_venicios@hotmail.com>
2020-02-10 22:48:25 -0300

    FilterOpenNow using class notation; style fixes

--- src/components/FilterOpenNow/filter-open-now.js
+++ src/components/FilterOpenNow/filter-open-now.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './filter-open-now.style.css';
 
-function FilterOpenNow() {
+class FilterOpenNow extends React.Component {
 
-  return (
-    <div className="filter open-now">
-      <input id='openNow' type='checkbox' />
-      <label htmlFor='openNow'>Open Now</label>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false,
+    };
+  }
+
+  check() {
+    this.setState({ checked: !this.state.checked });
+  }
+
+  render() {
+    return (
+      <div className='filter open-now' onClick={() => this.check()}>
+        <span className='circle-bordered'>
+          <span className={this.state.checked ? 'circle checked' : ''}></span>
+        </span>
+        <span className='label'>Open Now</span>
+      </div>
+    );
+  }
 }
 
 export default FilterOpenNow;
