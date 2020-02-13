e36632b4ead97fd718afc64a17e129505cde1fb6
KassioVenicios <kassio_venicios@hotmail.com>
2020-02-13 01:35:19 -0300

    dateFormat dd/MM/yyyy

--- src/components/RestaurantReviews/restaurant-reviews.js
+++ src/components/RestaurantReviews/restaurant-reviews.js
@@ -1,46 +1,53 @@
 import React from 'react';
 import StarRatings from '../StarRatings/star-ratings';
+import { ddMMyyy } from '../../utils/dateFormat';
 import './restaurant-reviews.style.css';
 
-function RestaurantReviews({ reviews }) {
+export default class RestaurantReviews extends React.Component {
 
-  return (
-    <section className='container reviews'>
-      <div className='counter'>
-        {reviews.length} Reviews
-      </div>
-      {
-        reviews.map((review, i) => (
-          <div key={review.id} className='review'>
-            <div className='reviewer'>
-              <figure>
-                <div style={styles(review.user.image_url)}></div>
-              </figure>
-              <div className='reviewer-info'>
-                <div className='reviewer-name'>{review.user.name}</div>
-                <div className='reviewer-date'>{review.time_created}</div>
+  render() {
+    const { reviews } = this.props;
+    return (
+      <section className='container reviews'>
+        <div className='counter'>
+          {reviews.length} Reviews
+        </div>
+        {
+          reviews.map((review, i) => (
+            <div key={review.id} className='review'>
+              <div className='reviewer'>
+                <figure>
+                  <div style={styles(review.user.image_url)}></div>
+                </figure>
+                <div className='reviewer-info'>
+                  <div className='reviewer-name'>
+                    {review.user.name}
+                  </div>
+                  <div className='reviewer-date'>
+                    {ddMMyyy(review.time_created)}</div>
+                </div>
               </div>
-            </div>
-            <div className='review-body'>
-              <StarRatings rating={review.rating}/>
-              <div className='message'>
-                {review.text}
+              <div className='review-body'>
+                <StarRatings rating={review.rating}/>
+                <div className='message'>
+                  {review.text}
+                </div>
               </div>
+              {
+                i !== reviews.length-1 ? (
+                  <hr style={{
+                    width: '100%',
+                    border: 'none',
+                    borderBottom: '1px solid #e6e6e6',
+                  }}/>
+                ) : null
+              }
             </div>
-            {
-              i !== reviews.length-1 ? (
-                <hr style={{
-                  width: '100%',
-                  border: 'none',
-                  borderBottom: '1px solid #e6e6e6',
-                }}/>
-              ) : null
-            }
-          </div>
-        ))
-      }
-    </section>
-  );
+          ))
+        }
+      </section>
+    );
+  }
 }
 
 const styles = photoUrl => ({
@@ -49,5 +56,3 @@ const styles = photoUrl => ({
   backgroundSize: 'cover',
   backgroundImage: `url(${photoUrl})`,
 });
-
-export default RestaurantReviews;
